1c37d0c00aded0d562063e0c377a0fa61c9975a3
Jules <Ccile@192.168.1.64>
2019-04-11 17:05:11 +0200

    create Spell Class

---
+++ S2-projet/src/fr/iutvalence/projet/battleArenaGame/Spell.java
@@ -0,0 +1,52 @@
+package fr.iutvalence.projet.battleArenaGame;
+/**
+ * 
+ * @author Jules
+ * represent a Spell who have one shape, a cooldown and an Effect
+ *
+ */
+public class Spell {
+	
+	private int currentCooldown;
+	private Shape myShape;
+	private Spell_Effect myEffect;
+	
+	/**
+	 * create a new empty Spell
+	 * 
+	 */
+	public Spell() {	}
+	
+	
+	/**	 
+	 * recover defaultCooldown in his shape
+	 * 
+	 */
+	public int getDefaultCooldown() {}
+	
+	/**
+	 * set a new value to currentCooldown
+	 * @param pcurrentCD new value of currentCooldown
+	 */
+	public void setCurrentCooldown(int pcurrentCD){}
+	
+	/**
+	 * return currentCooldown of the Spell
+	 * 
+	 */
+	public int getCurrentCooldown() {}
+	
+	/**
+	 * set a new value to myEffect
+	 * @param pEffectSpell_Effect is the new value of myEffect
+	 */
+	public void setSpellEffect(Spell_Effect pEffectSpell_Effect) {}
+	
+	/**
+	 * set a new value to myShape
+	 * @param pShape is the newvalue of myShape
+	 */
+	public void setShape(Shape pShape) {
+		
+	}
+}
