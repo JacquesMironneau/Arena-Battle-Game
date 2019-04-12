a7c354b0593ad46a595ccd8e29cba0d6e1bc140f
Isoke <thomas.durand152@gmail.com>
2019-04-12 18:26:48 +0200

    starting the documentation

--- S2-projet/src/fr/iutvalence/projet/battleArenaGame/Network.java
+++ S2-projet/src/fr/iutvalence/projet/battleArenaGame/Network.java
@@ -4,10 +4,14 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
+/**
+ * Represents the network of the game it enables the system to send and receive data
+ * @author durantho
+ *
+ */
 public class Network {
 	private Socket socket;
 	private InetAddress myAddr;
 	private ServerSocket lobby;
-	private int listening port;
+	private int listeningPort;
 }
