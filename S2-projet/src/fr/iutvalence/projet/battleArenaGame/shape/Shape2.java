12e17e152366c4bfb4fcfaff67f942373311d365
taki <thomas.durand152@gmail.com>
2019-05-08 16:08:24 +0200

    Add shape Enum named Shape2

---
+++ S2-projet/src/fr/iutvalence/projet/battleArenaGame/shape/Shape2.java
@@ -0,0 +1,60 @@
+package fr.iutvalence.projet.battleArenaGame.shape;
+
+import java.io.Serializable;
+import fr.iutvalence.projet.battleArenaGame.move.*;
+
+public enum Shape2 implements Serializable {
+	
+	Ball("Ball",0,2,5,3,new Coordinate[] {new Coordinate(0,0)}),
+	Fist("Fist",0,1,1,2,new Coordinate[] {new Coordinate(0,0)}),
+	Cross("Cross",0,3,5,4,new Coordinate[] {new Coordinate(0,0),new Coordinate(-2,0),new Coordinate(-1,0),new Coordinate(1,0),new Coordinate(2,0),new Coordinate(0,-2),new Coordinate(0,-1),new Coordinate(0,1),new Coordinate(0,2)}),
+	Square("Square",0,3,4,4,new Coordinate[] {new Coordinate(0,0),new Coordinate(0,-1),new Coordinate(0,1),new Coordinate(-1,0),new Coordinate(-1,-1),new Coordinate(-1,1),new Coordinate(1,0),new Coordinate(1,-1),new Coordinate(1,1)}),
+	Sword("Sword",0,2,1,3, new Coordinate[] {new Coordinate(-1,-1),new Coordinate(-1,0),new Coordinate(-1,1),new Coordinate(0,-1),new Coordinate(0,1),new Coordinate(1,-1),new Coordinate(1,0),new Coordinate(1,1)}),
+	Beam("Beam",0,3,1,4, new Coordinate[] {new Coordinate(0,1),new Coordinate(0,2),new Coordinate(0,3),new Coordinate(0,4),new Coordinate(0,5)});
+	
+	private String type;
+	private int damage;
+	private int cooldown;
+	private int range;
+	private int spellCost;
+	private Coordinate[] effectedCoordinates;
+	
+	Shape2(String type,int damage,int cooldown,int range,int spellCost, Coordinate[] effectedCoords){
+		this.type=type;
+		this.damage=damage;
+		this.cooldown=cooldown;
+		this.range=range;
+		this.spellCost=spellCost;
+		this.effectedCoordinates=effectedCoords;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public int getCooldown() {
+		return cooldown;
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	public int getSpellCost() {
+		return spellCost;
+	}
+
+	public Coordinate[] getEffectedCoordinates() {
+		return effectedCoordinates;
+	}
+	
+	
+
+	
+	
+	
+}
