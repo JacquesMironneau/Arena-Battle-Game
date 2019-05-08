76797759fc070e9aae307adf9b89edee6215ff1d
taki <thomas.durand152@gmail.com>
2019-05-08 17:00:16 +0200

    Add toString method to Shape enum, javadoc for the shape enum done

--- S2-projet/src/fr/iutvalence/projet/battleArenaGame/shape/Shape.java
+++ S2-projet/src/fr/iutvalence/projet/battleArenaGame/shape/Shape.java
@@ -3,6 +3,11 @@ package fr.iutvalence.projet.battleArenaGame.shape;
 import java.io.Serializable;
 import fr.iutvalence.projet.battleArenaGame.move.*;
 
+/**
+ * Shape Enumeration that represents all available types of shapes
+ * @author durantho
+ *
+ */
 public enum Shape implements Serializable {
 	
 	Ball("Ball",0,2,5,3,new Coordinate[] {new Coordinate(0,0)}),
@@ -12,13 +17,45 @@ public enum Shape implements Serializable {
 	Sword("Sword",0,2,1,3, new Coordinate[] {new Coordinate(-1,-1),new Coordinate(-1,0),new Coordinate(-1,1),new Coordinate(0,-1),new Coordinate(0,1),new Coordinate(1,-1),new Coordinate(1,0),new Coordinate(1,1)}),
 	Beam("Beam",0,3,1,4, new Coordinate[] {new Coordinate(0,1),new Coordinate(0,2),new Coordinate(0,3),new Coordinate(0,4),new Coordinate(0,5)});
 	
+	/**
+	 * Represents the type of the shape
+	 */
 	private String type;
+	/**
+	 * Represents the number of health points that the shape will remove
+	 */
 	private int damage;
+	
+	/**
+	 * Represents the cooldown of the shape in number of turns
+	 */
 	private int cooldown;
+	
+	/**
+	 * Represents the range of the shape, to make it simple it's how far you can click from the pawn
+	 */
 	private int range;
+	
+	/**
+	 * Represents the cost in action points,
+	 * It will effect the cost of a spell
+	 */
 	private int spellCost;
+	
+	/**
+	 * It represents the cells that the shape will effect
+	 */
 	private Coordinate[] effectedCoordinates;
 	
+	/**
+	 * 
+	 * @param type the type of the Shape
+	 * @param damage the damage that the shape will inflict
+	 * @param cooldown the cooldown of the shape
+	 * @param range the range of the shape
+	 * @param spellCost the cost of the shape in action points
+	 * @param effectedCoords the coordinate that will be effect by the shape
+	 */
 	Shape(String type,int damage,int cooldown,int range,int spellCost, Coordinate[] effectedCoords){
 		this.type=type;
 		this.damage=damage;
@@ -28,30 +65,58 @@ public enum Shape implements Serializable {
 		this.effectedCoordinates=effectedCoords;
 	}
 
+	/**
+	 * Getter for the type of shape
+	 * @return the type of the shape
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * Getter for the damages of the shape
+	 * @return the damage that the shape will inflict
+	 */
 	public int getDamage() {
 		return damage;
 	}
 
+	/**
+	 * Getter for the cooldown of the shape
+	 * @return the reloading time of the shape in turns
+	 */
 	public int getCooldown() {
 		return cooldown;
 	}
 
+	/**
+	 * Getter for the range of the shape
+	 * @return the range in cells (how far you can cast the spell from your pawn
+	 */
 	public int getRange() {
 		return range;
 	}
 
+	/**
+	 * Getter for the spellcost of the shape
+	 * @return the cost of the shape in action points
+	 */
 	public int getSpellCost() {
 		return spellCost;
 	}
 
+	/**
+	 * Getter for the effectedCoordinates of the shape
+	 * @return the array of coordinates that the shape will effect
+	 */
 	public Coordinate[] getEffectedCoordinates() {
 		return effectedCoordinates;
 	}
 	
+	/**
+	 * Displays the coordinates clearly
+	 * @return each coordinates that the shape will effect
+	 */
 	public String displayEffectedCoords() {
 		int k;
 		String effectedCoords = "[ ";
@@ -62,6 +127,9 @@ public enum Shape implements Serializable {
 		return effectedCoords;
 	}
 	
+	/**
+	 * Displays the actual state of the shape
+	 */
 	public String toString() {
 		return "Shape [type="+this.type+",damage="+this.damage+",cooldown="+this.cooldown+",range="+this.range+",spellCost="+this.cooldown+"effectedCoordinates="+this.displayEffectedCoords()+"]";
 	}
