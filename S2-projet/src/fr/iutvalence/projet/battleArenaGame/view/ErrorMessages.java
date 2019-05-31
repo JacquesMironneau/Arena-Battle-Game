cdce319003c82f615c4d5c8b26cea9587b97737b
Pashmi <lowneonema@gmail.com>
2019-05-31 22:35:28 +0200

    Add OutofRange error in ErrorMessages

--- S2-projet/src/fr/iutvalence/projet/battleArenaGame/view/ErrorMessages.java
+++ S2-projet/src/fr/iutvalence/projet/battleArenaGame/view/ErrorMessages.java
@@ -9,10 +9,13 @@ public enum ErrorMessages
 {
 	MOVE_OUT_OF_RANGE("You don't have enough point to move"),
 	MOVE_OUT_OF_BOARD("Your target location is outside the board..."),
-	NOT_ENOUGH_ACTION_POINT("You need more AP in order to cast this spell !"),
 	NOT_ENOUGH_MOVE_POINT("You need more MP in order to move to this location !"),
-	SPELL_IN_COOLDOWN("This spell is still in cooldown !"),
 	CASE_OCCUPATED("This location is unavailable"),
+	
+	NOT_ENOUGH_ACTION_POINT("You need more AP in order to cast this spell !"),
+	SPELL_IN_COOLDOWN("This spell is still in cooldown !"),
+	SPELL_TARGET_OUT_OF_RANGE("Your aiming too far !"),
+	
 	PAWN_NO_TEAM("This pawn is not remote or local..."),
 	SYSTEM_ERROR("Error system");
 	
