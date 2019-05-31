d99ed52dc5519bfeb5c344eb4560bf1cbd75d753
Pashmi <lowneonema@gmail.com>
2019-05-31 22:31:35 +0200

    Add ErrorMessage handling with an enumeration

---
+++ S2-projet/src/fr/iutvalence/projet/battleArenaGame/view/ErrorMessages.java
@@ -0,0 +1,31 @@
+package fr.iutvalence.projet.battleArenaGame.view;
+/**
+ * This stands for every error the player can face
+ * used to display in the view (HMI)
+ * @author pashmi
+ *
+ */
+public enum ErrorMessages
+{
+	MOVE_OUT_OF_RANGE("You don't have enough point to move"),
+	MOVE_OUT_OF_BOARD("Your target location is outside the board..."),
+	NOT_ENOUGH_ACTION_POINT("You need more AP in order to cast this spell !"),
+	NOT_ENOUGH_MOVE_POINT("You need more MP in order to move to this location !"),
+	SPELL_IN_COOLDOWN("This spell is still in cooldown !"),
+	CASE_OCCUPATED("This location is unavailable"),
+	PAWN_NO_TEAM("This pawn is not remote or local..."),
+	SYSTEM_ERROR("Error system");
+	
+	private String errMessage;
+	
+	ErrorMessages(String s)
+	{
+		this.errMessage = s;
+	}
+	
+	public String getErrorMessage()
+	{
+		return this.errMessage;
+	}
+	
+}
