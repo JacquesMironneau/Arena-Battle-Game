6a4b02f7f2f5d4fbc11c7b586084fe3ff96bc21d
jules chapelle <jules chapelle@LAPTOP-GNEHCQ45>
2019-06-04 11:58:06 +0100

    fix new problem in BoardTest and test for new method

--- S2-projet/src/fr/iutvalence/projet/battleArenaGame/test/BoardTest.java
+++ S2-projet/src/fr/iutvalence/projet/battleArenaGame/test/BoardTest.java
@@ -2,22 +2,20 @@ package fr.iutvalence.projet.battleArenaGame.test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
 import fr.iutvalence.projet.battleArenaGame.Board;
-import fr.iutvalence.projet.battleArenaGame.EndStatus;
 import fr.iutvalence.projet.battleArenaGame.Game;
+import fr.iutvalence.projet.battleArenaGame.exceptions.SpellIndexException;
 import fr.iutvalence.projet.battleArenaGame.move.Coordinate;
-import fr.iutvalence.projet.battleArenaGame.move.Movement;
 import fr.iutvalence.projet.battleArenaGame.network.Local;
 import fr.iutvalence.projet.battleArenaGame.pawn.Pawn;
 import fr.iutvalence.projet.battleArenaGame.pawn.PawnEffect;
-import fr.iutvalence.projet.battleArenaGame.pawn.PawnTeam;
 import fr.iutvalence.projet.battleArenaGame.shape.Shape;
+import fr.iutvalence.projet.battleArenaGame.spell.Spell;
 import fr.iutvalence.projet.battleArenaGame.spell.SpellEffect;
 import fr.iutvalence.projet.battleArenaGame.spell.SpellPage;
 import fr.iutvalence.projet.battleArenaGame.view.PlayerConsole;
@@ -29,13 +27,16 @@ class BoardTest
 	@Test
 	void testCheckMove()
 	{
+		Game.maxPlayer=2;
 		PlayerConsole P1 = new PlayerConsole();
 		Game G1 = new Game(P1);
 		Board b = new Board(new Local(G1), P1);
-		Coordinate coord = new Coordinate(1,1);
+		int oldX=b.getTurnOrder().get(b.getCurrentPawnIndex()).getPos().getCoordX();
+		int oldY = b.getTurnOrder().get(b.getCurrentPawnIndex()).getPos().getCoordY();
+		Coordinate coord = new Coordinate(oldX+1,oldY);
 		b.checkMove(coord);
-		assertTrue("mauvais mouvement X",b.getTurnOrder().get(b.getCurrentPawnIndex()).getPos().getCoordX()==1);
-		assertTrue("mauvais mouvement y",b.getTurnOrder().get(b.getCurrentPawnIndex()).getPos().getCoordY()==1);
+		assertTrue("mauvais mouvement X",b.getTurnOrder().get(b.getCurrentPawnIndex()).getPos().getCoordX()==oldX+1);
+		assertTrue("mauvais mouvement y",b.getTurnOrder().get(b.getCurrentPawnIndex()).getPos().getCoordY()==oldY);
 		coord = new Coordinate(100,100);
 		b.checkMove(coord);
 		assertFalse("gestion de distance",b.getTurnOrder().get(b.getCurrentPawnIndex()).getPos().getCoordX()==100);
@@ -49,8 +50,10 @@ class BoardTest
 	@Test
 	void testApplyEffet() //Tested entirely
 	{
-		
-		Board b = new Board(null, null);
+		Game.maxPlayer=2;
+		PlayerConsole P1 = new PlayerConsole();
+		Game G1 = new Game(P1);
+		Board b = new Board(new Local(G1), P1);
 		System.out.println(b.getTurnOrder().get(b.getCurrentPawnIndex()));
 		//primary tests
 		
@@ -75,7 +78,9 @@ class BoardTest
 
 		
 		//Update element test
-		b = new Board(null, null);
+		 P1 = new PlayerConsole();
+		G1 = new Game(P1);
+		b = new Board(new Local(G1), P1);
 		b.getTurnOrder().get(b.getCurrentPawnIndex()).addEffect(new PawnEffect(SpellEffect.Fire));
 		b.applyEffect();
 		assertFalse("Effect disapears", b.getTurnOrder().get(b.getCurrentPawnIndex()).getEffect().size()==0);
@@ -117,11 +122,12 @@ class BoardTest
 	@Test
 	void testCheckSpell()
 	{
+		Game.maxPlayer=2;
 		PlayerConsole P1 = new PlayerConsole();
 		Game G1 = new Game(P1);
 		Board b = new Board(new Local(G1), P1);
 		Coordinate coord = new Coordinate(1,1);
-SpellPage page1 = new SpellPage("Namepage1");
+		SpellPage page1 = new SpellPage("Namepage1");
 		
 		Shape fist = Shape.Fist;
 		Shape ball = Shape.Ball;
@@ -135,12 +141,13 @@ SpellPage page1 = new SpellPage("Namepage1");
 		page1.getSpell(0).setSpellEffect(anEffect);
 		coord = new Coordinate(1,2);
 		b.getTurnOrder().get(b.getCurrentPawnIndex()).setSpellPage(page1);
-		b.checkSpell(0, coord);
-		assertFalse("getion de portée",b.getTurnOrder().get(b.getCurrentPawnIndex()).getSpellPage().getSpell(0).getCurrentCooldown()==b.getTurnOrder().get(b.getCurrentPawnIndex()).getSpellPage().getSpell(0).getDefaultCooldown());
-		//mov = new Movement(new Coordinate(0,0),new Coordinate(1,0));
+		b.checkSpell(1, coord);
+		assertFalse("getion de portée",b.getTurnOrder().get(b.getCurrentPawnIndex()).getSpellPage().getSpell(1).getCurrentCooldown()==b.getTurnOrder().get(b.getCurrentPawnIndex()).getSpellPage().getSpell(1).getDefaultCooldown());
+		
 		Coordinate c1 = new Coordinate(b.getTurnOrder().get(b.getCurrentPawnIndex()).getPos().getCoordX()+1,b.getTurnOrder().get(b.getCurrentPawnIndex()).getPos().getCoordY());
 		b.checkSpell(0, c1);
 		assertTrue("gestion range",b.getTurnOrder().get(b.getCurrentPawnIndex()).getSpellPage().getSpell(0).getCurrentCooldown()==b.getTurnOrder().get(b.getCurrentPawnIndex()).getSpellPage().getSpell(0).getDefaultCooldown());
+		
 		b.checkSpell(0, c1);
 		assertTrue("gestion range",b.getTurnOrder().get(b.getCurrentPawnIndex()).getSpellPage().getSpell(0).getCurrentCooldown()==b.getTurnOrder().get(b.getCurrentPawnIndex()).getSpellPage().getSpell(0).getDefaultCooldown());
 		b.getTurnOrder().get(b.getCurrentPawnIndex()).setActionPoints(0);
@@ -157,8 +164,10 @@ SpellPage page1 = new SpellPage("Namepage1");
 	@Test
 	void testNextPawn()
 	{
-
-		Board b = new Board(null, null);
+		Game.maxPlayer=2;
+		PlayerConsole P1 = new PlayerConsole();
+		Game G1 = new Game(P1);
+		Board b = new Board(new Local(G1), P1);
 		Pawn previousPawn = b.getTurnOrder().get(b.getCurrentPawnIndex());
 		b.nextPawn();
 		assertFalse("Suivant de P1 = p1", previousPawn == b.getTurnOrder().get(b.getCurrentPawnIndex()));
@@ -179,7 +188,10 @@ SpellPage page1 = new SpellPage("Namepage1");
 	@Test
 	void testRemoveDead()
 	{
-		Board b = new Board(null, null);
+		Game.maxPlayer=2;
+		PlayerConsole P1 = new PlayerConsole();
+		Game G1 = new Game(P1);
+		Board b = new Board(new Local(G1), P1);
 		//(b.getTurnOrder().get(b.getCurrentPawnIndex()).setHealthPoints(10);
 		System.out.println(b.getTurnOrder().size());
 		assertTrue("Turn order correct", b.getTurnOrder().size()==6);
@@ -195,7 +207,9 @@ SpellPage page1 = new SpellPage("Namepage1");
 		//System.out.println(b.getTurnOrder().size());
 		assertTrue("Turn order non vide", b.getTurnOrder().isEmpty());
 		
-		b = new Board(null, null);
+		P1 = new PlayerConsole();
+		G1 = new Game(P1);
+		b = new Board(new Local(G1), P1);
 		b.getTurnOrder().get(b.getCurrentPawnIndex()).setHealthPoints(10);
 		assertFalse("Turn order non vide", b.getTurnOrder().isEmpty());
 
@@ -204,8 +218,46 @@ SpellPage page1 = new SpellPage("Namepage1");
 	}
 	
 	@Test
-	void testAre()
+	void testAreAllPageSet() throws SpellIndexException {
+		Game.maxPlayer=2;
+		PlayerConsole P1 = new PlayerConsole();
+		Game G1 = new Game(P1);
+		Board b = new Board(new Local(G1), P1);
+		assertFalse("pas de page de sort inisialisé",b.areAllPageSet());
+		SpellPage p1 = new SpellPage("page1");
+		Spell s1 = new Spell();
+		Spell s2 = new Spell();
+		Spell s3 = new Spell();
+		s1.setShape(Shape.Ball);
+		s2.setShape(Shape.Fist);
+		s3.setShape(Shape.Square);
+		s1.setSpellEffect(SpellEffect.Fire);
+		s2.setSpellEffect(SpellEffect.Ice);
+		s3.setSpellEffect(SpellEffect.Fire);
+		
+		p1.setSpell(0,s1);
+		p1.setSpell(1,s2);
+		p1.setSpell(2,s3);
+		
+		b.getTurnOrder().get(0).setSpellPage(p1);
+		b.getTurnOrder().get(1).setSpellPage(p1);
+		b.getTurnOrder().get(2).setSpellPage(p1);
+		assertFalse("pas toutes les pages de sort inisialisé",b.areAllPageSet());
+		b.getTurnOrder().get(3).setSpellPage(p1);
+		b.getTurnOrder().get(4).setSpellPage(p1);
+		b.getTurnOrder().get(5).setSpellPage(p1);
+		assertTrue("toutes les pages de sort inisialisé",b.areAllPageSet());
+	}
 	
-	
+	@Test
+	void testBoard() {
+		Game.maxPlayer=2;
+		PlayerConsole P1 = new PlayerConsole();
+		Game G1 = new Game(P1);
+		Board b = new Board(new Local(G1), P1);
+		assertTrue("page de sort non vide",b.getTurnOrder().get(0).getSpellPage()==null);
+		assertTrue("nombre de pion",b.getTurnOrder().size()==6);
+		assertTrue("valeur nbPawn",b.getNbPawn()==3);
+	}
 	
 }
