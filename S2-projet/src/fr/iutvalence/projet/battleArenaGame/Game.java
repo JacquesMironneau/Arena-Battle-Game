de1b878eac8205f96e55b3027cfc38695567041f
charvevi <vincent.charvet@etu.univ-grenoble-alpes.fr>
2019-06-06 16:29:36 +0200

    Review JavaDoc and remove useless methods

--- S2-projet/src/fr/iutvalence/projet/battleArenaGame/Game.java
+++ S2-projet/src/fr/iutvalence/projet/battleArenaGame/Game.java
@@ -15,70 +15,45 @@ import fr.iutvalence.projet.battleArenaGame.view.PlayerConsole;
 
 /**
  * Game class stands for the system of the BattleArena Game:
- * It handles the players, the network, the spellpages of the player, the coordinate
- * and manage turns for every player. 
- * Here the game starts and end
- * 
- * @author mironnej
- *
+ * It handles the players IHM, the network, the board and manage turns
  */
-
 public class Game implements GameController
 {
-	
-	
-    /**
-     * Used in the network : Message send by a Client to left the application
-     */
-    public final static String QUIT = "quit";
-	
-    /**
-     * Used in the network: Port 
-     */
-    
-    public final static int PORT = 12899;
-    
-    /**
-     * Used in the network: IP address of the server (might be deleted when UDP auto IP will be implemented
-     */
-    
-    public final static String HOST_ADDRESS = "192.168.0.16";
-    
-    /**
-	 * Used in the network : Numbers of player (excluding the host)
-	 */
+	//Attributes 
+   /**
+    * Number of players in the game
+    */
     private int maxPlayer;
     
-	
 	/**
-	 * First player of the game, the one who start in the first turn
+	 * List of all player's IHM in the game
 	 */
 	private ArrayList<GameView> players;
 	
-	
 	/**
-	 * Board of the game
+	 * The board of the game
 	 */
 	private Board board;
 	
 	/**
-	 * List of all spellPages of the player
+	 * List of all spellPages in the game
 	 */
+	//TODO Will be changed to allow players to join a game with their own spell pages list and only use those
 	private ArrayList<SpellPage> mySpellPages;
-		
 	
 	/**
-	 * Id of the player who win the game
+	 * Status of the Game
+	 * Is Running if the game is not ended, Draw if there is no winner and Victory if a player win
 	 */
-	private int winnerID;
+	private EndStatus gameStatus;
 	
-
-	
-	
-	
+	//Constructor
 	/**
-	 * Constructor for Game
-	 * create the game, and call init to initialize the board.
+	 * Create a Game parameterized by the number of player, the number of pawn for each player
+	 * and the board size wanted by the player who create the game.
+	 * @param nbPlayer is the number of player wanted for this game
+	 * @param nbPawn is the number of pawn for each player wanted for this game
+	 * @param boardSize is the size of the board
 	 */
 	public Game(int nbPlayer,int nbPawn,int boardSize)
 	{
@@ -94,20 +69,20 @@ public class Game implements GameController
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-			
-		
-		}
+		this.gameStatus = EndStatus.RUNNING;		
+	}
 	
+	//Methods
 	
-	/**TODO update this doc
-	 * Manage method for turns
-	 * First the play method check if game is ended: No more pawns in one team player.
-	 * It first select the currentPawn
-	 * Here we allow player to choose actions for the currentPawn.
-	 * There's a loop where the current player can ask for movement, spell casting.
-	 * Then the system check if theses move or cast are authorized then it does the job.
-	 * When the player ask to end his turn, the system switch to next player with a new currentPawn.
-	 * 
+	/**
+	 * This is the algorithm of a game
+	 * First it starts the selection of spell pages for pawns phase.
+	 * Then it enters in the turn management loop which ask to the player to choose between 3 actions :
+	 * -Move : Ask a move to the player for his pawn
+	 * -Launch : Ask to the player the spell he wants to cast and where
+	 * -EndTurn : End his turn
+	 * The player can do an unlimited number of action in his turn while he has enough points to perform them
+	 * It exist the turn loop if the game if finished 
 	 */
 	
 	public void play()
@@ -123,11 +98,10 @@ public class Game implements GameController
 			this.players.get(currentPlayerIndex).displaySelectForThisPawn(this.board.getTurnOrder().get(this.board.getCurrentPawnIndex()));
 			this.players.get(currentPlayerIndex).askPageSelection(currentPlayerIndex);
 			this.board.nextPawn();
+			//If the last pawn of the list has a page, which means that all pawns have pages
 			if(!(this.board.getTurnOrder().get(this.board.getTurnOrder().size()-1).getSpellPage()==null))
 				break;
-	//TODO fix this, because it never break the loop : the page is not set when it's the last to set		
 		}
-		
 		
 		//Turn algorithm
 		currentPlayerIndex = 0;
@@ -148,10 +122,8 @@ public class Game implements GameController
 						this.board.nextPawn();
 						break;
 					}
-					if(endGame())
-						{
+					if(endGame()!=EndStatus.RUNNING)
 						break;
-						}
 					this.players.get(currentPlayerIndex).displayChoiceAction();
 					switch(this.players.get(currentPlayerIndex).askActionChoice())
 					{
@@ -160,9 +132,7 @@ public class Game implements GameController
 						this.players.get(currentPlayerIndex).displayMoveSelection();
 						this.players.get(currentPlayerIndex).askMove(currentPlayerIndex);
 						for(GameView gv : players)
-						{
 							gv.displayBoard(board,this.maxPlayer);
-						}
 						break;
 					case LAUNCH_SPELL:
 						this.players.get(currentPlayerIndex).displayBoard(board,this.maxPlayer);
@@ -170,243 +140,156 @@ public class Game implements GameController
 						this.players.get(currentPlayerIndex).displaySpellSelection();
 						this.players.get(currentPlayerIndex).askSpell(currentPlayerIndex);
 						if(this.board.getTurnOrder().get(this.board.getCurrentPawnIndex()).getHealthPoints()<=0)
-						{
 							this.board.nextPawn();
-						}
 						this.board.removeDeads();
-						if(endGame())
+						if(endGame() != EndStatus.RUNNING)
 							break;
 						for(GameView gv : players)
-						{
 							gv.displayBoard(board,this.maxPlayer);
-						}
 						break;
 					case END_TURN:
 						this.board.nextPawn();
 						break;
 					}
-					if(endGame())
-					{
+					if(endGame() != EndStatus.RUNNING)
 						break;
-					}
 					for(GameView gv : players)
-					{
 						gv.displayNextTurn(currentPlayerIndex+1);
-					}
-				}
-		if(this.winnerID==-1)
-		{
-			for(GameView gv : players)
-			{
-				gv.displayEnd("Tout le monde est mort ! Egalité !");
 			}
-		}
+		if(gameStatus == EndStatus.DRAW)
+			for(GameView gv : players)
+				gv.displayEnd("Tout le monde est mort ! Egalité !");
 		else
 			for(GameView gv : players)
-			{
-				gv.displayEnd("Victoire du joueur " + this.winnerID + "! GG !");
-			}
-		//closeGame();
+				gv.displayEnd("Victoire du joueur " + this.board.getTurnOrder().get(0).getTeamId() + "! GG !");
 	}
 	
 	
+	
 	/**
-	 * TODO Add theses changes in the Network class
-	 * Might Disconnect the user (myClient.disconnect() and the Server ( myServer.disconnectAll())e
+	 * Check if the game is ended
+	 * @return an EndStatus corresponding to the state of the game
 	 */
-	public boolean endGame()
+	public EndStatus endGame()
 	{
 		if(this.board.getTurnOrder().size()==0)
-			{
-			this.winnerID = -1;
-			return true;
-			}
+			return EndStatus.DRAW;
+		
 		if(this.board.getTurnOrder().size()<= this.board.getNbPawn())
 		{
 			for(Pawn p : this.board.getTurnOrder())
 				if(p.getTeamId()!=this.board.getTurnOrder().get(0).getTeamId())
-					return false;			
-			return true;
+					return EndStatus.RUNNING;			
+			return EndStatus.VICTORY;
 		}
-		return false;
+		return EndStatus.RUNNING;
 	}
 	
-	
-	/**
-	 * This method close the game
-	 * Called when a the game is finished, after results
-	 */
-	private void closeGame()
+	public  void setNbPlayer(int nbPlayers)
 	{
-		System.exit(1); // This stop in a clean way the application
+		this.maxPlayer = nbPlayers;
+	}
+
+	@Override
+	public void moveRequest(int currentPlayerIndex,Coordinate destination)
+	{
+		switch(this.board.checkMove(destination))
+		{
+		case MOVE_OUT_OF_RANGE:
+			this.players.get(currentPlayerIndex).displayStatus(StatusMessages.MOVE_OUT_OF_RANGE);
+			break;
+			
+		case MOVE_OUT_OF_BOARD:
+			this.players.get(currentPlayerIndex).displayStatus(StatusMessages.MOVE_OUT_OF_BOARD);
+			break;
+			
+		case CASE_OCCUPATED:
+			this.players.get(currentPlayerIndex).displayStatus(StatusMessages.CASE_OCCUPATED);
+			break;
+			
+		case MOVE_DONE:
+			this.players.get(currentPlayerIndex).displayStatus(StatusMessages.MOVE_DONE);
+			break;
+		}
+	}
+
+	@Override
+	public void spellRequest(int currentPlayerIndex,int spellIndex,Coordinate destination)
+	{
+		switch(this.board.checkSpell(spellIndex, destination))
+		{
+		case WRONG_INDEX:
+			this.players.get(currentPlayerIndex).displayStatus(StatusMessages.WRONG_INDEX);
+			break;
+			
+		case SPELL_IN_COOLDOWN:  
+			this.players.get(currentPlayerIndex).displayStatus(StatusMessages.SPELL_IN_COOLDOWN);
+			break;
+			
+		case NOT_ENOUGH_ACTION_POINT:
+			this.players.get(currentPlayerIndex).displayStatus(StatusMessages.NOT_ENOUGH_ACTION_POINT);
+			break;
+			
+		case SPELL_TARGET_OUT_OF_RANGE:
+			this.players.get(currentPlayerIndex).displayStatus(StatusMessages.SPELL_TARGET_OUT_OF_RANGE);
+			break;
+			
+		case SPELL_SENT:
+			this.players.get(currentPlayerIndex).displayStatus(StatusMessages.SPELL_SENT);
+			break;
+		}
+	}
 	
+	@Override
+	//TODO add answer messages
+	public void setPageRequest(int currentPlayerIndex,int pageToSet)
+	{
+		for(Pawn p : this.board.getTurnOrder())
+			if(p.getTeamId()==currentPlayerIndex && p.getSpellPage() == null)
+				{
+				p.setSpellPage(new SpellPage(this.getSpellPages().get(pageToSet)));
+				this.players.get(currentPlayerIndex).displayStatus(StatusMessages.PAGE_SET);
+				break;
+				}
 	}
 	
 	/**
-	 * Getter for spell pages
-	 * @return
+	 * Create a filled SpellPage
+	 * @throws SpellIndexException
 	 */
+	//TODO Remove this method, used for test
+	public void createSpellPageForTest() throws SpellIndexException
+	{
+		SpellPage p1 = new SpellPage("page1");
+		Spell s1 = new Spell();
+		Spell s2 = new Spell();
+		Spell s3 = new Spell();
+		s1.setShape(Shape.Ball);
+		s2.setShape(Shape.Fist);
+		s3.setShape(Shape.Square);
+		s1.setSpellEffect(SpellEffect.Fire);
+		s2.setSpellEffect(SpellEffect.Ice);
+		s3.setSpellEffect(SpellEffect.Fire);
+		
+		p1.setSpell(0,s1);
+		p1.setSpell(1,s2);
+		p1.setSpell(2,s3);
+		
+		this.mySpellPages.add(p1);
+	}
+
+	//Getters
 	public ArrayList<SpellPage> getSpellPages()
 	{
 		return this.mySpellPages;
 	}
 	
-	/**
-	 * @return the board of the current Game
-	 */
 	public Board getBoard()
 	{
 		return this.board;
 	}
 	
-	/**
-	 * Creation of a spellPage using player HMI selection 
-	 */
-	//TODO fix that and think about how and when players can create their page, think how to keep pages saved
-	//and how players access to their own pages only
-/*	public void createSpellPage()
-	{
-		SpellPage pageToAdd = new SpellPage(this.localPlayer.askPageName());
-		try {
-			pageToAdd.setSpell(0,null);
-			pageToAdd.setSpell(1,null);
-			pageToAdd.setSpell(2,null);
-		} catch (SpellIndexException e1) {
-			e1.printStackTrace();
-		}
-		boolean pageFinished = false;
-		while(!pageFinished)
-		{
-		Spell spellToAdd = new Spell();
-		this.localPlayer.displaySpellPageDetail(pageToAdd);
-		int indexToAdd = this.localPlayer.askSpell();
-		spellToAdd.setSpellEffect(this.localPlayer.askSpellElement());
-		spellToAdd.setShape(this.localPlayer.askSpellShape(spellToAdd.getSpellEffect()));
-		try {
-			pageToAdd.setSpell(indexToAdd,spellToAdd);
-		} catch (SpellIndexException e) {
-			System.out.println("C'est mort");
-			e.printStackTrace();
-		}
-		if(pageToAdd.getSpell(0)!= null && pageToAdd.getSpell(1)!= null && pageToAdd.getSpell(2)!= null)
-			pageFinished = this.localPlayer.askValidation();
-		
-		}
-		this.mySpellPages.add(pageToAdd);
+	public int getMaxPlayer() {
+		return maxPlayer;
 	}
-	
-	
-	public synchronized Set<TeamId> getMyIds() 
-	{
-		return this.myIds;
-	}*/
-/**
- * Used for test
- * Create a spell page with 3 spells
- * @throws SpellIndexException 
- *
- */
-	//TODO Remove this method, used for test
-public void createSpellPageForTest() throws SpellIndexException
-{
-	SpellPage p1 = new SpellPage("page1");
-	Spell s1 = new Spell();
-	Spell s2 = new Spell();
-	Spell s3 = new Spell();
-	s1.setShape(Shape.Ball);
-	s2.setShape(Shape.Fist);
-	s3.setShape(Shape.Square);
-	s1.setSpellEffect(SpellEffect.Fire);
-	s2.setSpellEffect(SpellEffect.Ice);
-	s3.setSpellEffect(SpellEffect.Fire);
-	
-	p1.setSpell(0,s1);
-	p1.setSpell(1,s2);
-	p1.setSpell(2,s3);
-	
-	this.mySpellPages.add(p1);
-}
-
-public void setBoard(Board b) {
-	this.board=b;	
-
-}
-
-
-
-public int getMaxPlayer() {
-	return maxPlayer;
-}
-
-
-public  void setNbPlayer(int nbPlayers)
-{
-	this.maxPlayer = nbPlayers;
-}
-
-
-public void moveRequest(int currentPlayerIndex,Coordinate destination)
-{
-	switch(this.board.checkMove(destination))
-	{
-	case MOVE_OUT_OF_RANGE:
-		this.players.get(currentPlayerIndex).displayStatus(StatusMessages.MOVE_OUT_OF_RANGE);
-		break;
-		
-	case MOVE_OUT_OF_BOARD:
-		this.players.get(currentPlayerIndex).displayStatus(StatusMessages.MOVE_OUT_OF_BOARD);
-		break;
-		
-	case CASE_OCCUPATED:
-		this.players.get(currentPlayerIndex).displayStatus(StatusMessages.CASE_OCCUPATED);
-		break;
-		
-	case MOVE_DONE:
-		this.players.get(currentPlayerIndex).displayStatus(StatusMessages.MOVE_DONE);
-		break;
-	}
-}
-
-public void spellRequest(int currentPlayerIndex,int spellIndex,Coordinate destination)
-{
-	switch(this.board.checkSpell(currentPlayerIndex, spellIndex, destination))
-	{
-	case WRONG_INDEX:
-		this.players.get(currentPlayerIndex).displayStatus(StatusMessages.WRONG_INDEX);
-		break;
-		
-	case SPELL_IN_COOLDOWN:  
-		this.players.get(currentPlayerIndex).displayStatus(StatusMessages.SPELL_IN_COOLDOWN);
-		break;
-		
-	case NOT_ENOUGH_ACTION_POINT:
-		this.players.get(currentPlayerIndex).displayStatus(StatusMessages.NOT_ENOUGH_ACTION_POINT);
-		break;
-		
-	case SPELL_TARGET_OUT_OF_RANGE:
-		this.players.get(currentPlayerIndex).displayStatus(StatusMessages.SPELL_TARGET_OUT_OF_RANGE);
-		break;
-		
-	case SPELL_SENT:
-		this.players.get(currentPlayerIndex).displayStatus(StatusMessages.SPELL_SENT);
-		break;
-	}
-	
-}
-
-//TODO add answer messages, will be usefull to check if the bug happens here
-public void setPageRequest(int currentPlayerIndex,int pageToSet)
-{
-	for(Pawn p : this.board.getTurnOrder())
-		if(p.getTeamId()==currentPlayerIndex && p.getSpellPage() == null)
-			{
-			p.setSpellPage(new SpellPage(this.getSpellPages().get(pageToSet)));
-			this.players.get(currentPlayerIndex).displayStatus(StatusMessages.PAGE_SET);
-			break;
-			}
-
-}
-
-
-
 }
--- S2-projet/src/fr/iutvalence/projet/battleArenaGame/GameController.java
+++ S2-projet/src/fr/iutvalence/projet/battleArenaGame/GameController.java
@@ -1,12 +1,31 @@
 package fr.iutvalence.projet.battleArenaGame;
 
 import fr.iutvalence.projet.battleArenaGame.move.Coordinate;
-import fr.iutvalence.projet.battleArenaGame.spell.SpellPage;
-
+/**
+ * Contains all methods that the player can call to affect the game
+ */
 public interface GameController
 {
+	/**
+	 * Send a request for a move
+	 * @param currentPlayerIndex is the index in the players list of the player which is currently playing
+	 * @param destination is the destination that the player wants to reach
+	 */
 	public void moveRequest(int currentPlayerIndex,Coordinate destination);
+	
+	/**
+	 * Send a request to cast a spell
+	 * @param currentPlayerIndex is the index in the players list of the player which is currently playing
+	 * @param spellIndex is the index of the spell in the spell page of the current pawn that the player wants to cast
+	 * @param destination is the target of the spell that the player wants to hit
+	 */
 	public void spellRequest(int currentPlayerIndex,int spellIndex,Coordinate destination);
+	
+	/**
+	 * Send a request to set a spell page to the current paw
+	 * @param currentPlayerIndex is the index in the players list of the player which is currently playing
+	 * @param pageToSet the spell page to set to the pawnDO
+	 */
 	//TODO replace (int)pageToSet by a SpellPage. The problem is that we need to access to the player's page in the GameView actually.
 	//The solution may be to send spellPages with the configurable part of the application which will be worked later.
 	public void setPageRequest(int currentPlayerIndex,int pageToSet);
