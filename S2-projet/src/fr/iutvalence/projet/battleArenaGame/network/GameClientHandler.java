857051ae4d31d6a74030070e8f8dfeb04a8e3b25
Pashmi <lowneonema@gmail.com>
2019-06-11 16:58:05 +0200

    Edit GameView methods and rework network

--- S2-projet/src/fr/iutvalence/projet/battleArenaGame/network/GameClientHandler.java
+++ S2-projet/src/fr/iutvalence/projet/battleArenaGame/network/GameClientHandler.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import fr.iutvalence.projet.battleArenaGame.Board;
 import fr.iutvalence.projet.battleArenaGame.GameController;
+import fr.iutvalence.projet.battleArenaGame.move.Coordinate;
 import fr.iutvalence.projet.battleArenaGame.pawn.Pawn;
 import fr.iutvalence.projet.battleArenaGame.spell.Effect;
 import fr.iutvalence.projet.battleArenaGame.spell.SpellPage;
@@ -13,13 +14,14 @@ import fr.iutvalence.projet.battleArenaGame.view.StatusMessages;
 
 public class GameClientHandler implements GameView
 {
+	public static final String SEPARATOR = "~~~~";
+	
 	private GameController gameController;
 	
-	private ClientConnectionInfo[] clients;
+	private ClientConnectionInfo clients;
 	
-	public GameClientHandler(GameController gc, ClientConnectionInfo[] clientsInfo)
+	public GameClientHandler(ClientConnectionInfo clientsInfo)
 	{
-		this.gameController = gc;
 		this.clients = clientsInfo;
 
 		new Thread(() ->  {
@@ -29,19 +31,43 @@ public class GameClientHandler implements GameView
 		
 	}
 	
+	public void setGameController(GameController gc)
+	{
+		this.gameController = gc;
+	}
+	
 	public void receive()
 	{
 		while(true) //TODO care for the infinite loop
 		{
-			for(ClientConnectionInfo client: this.clients)
-			{
+			String received = null;
+
 				try {
-					client.getReader().readLine();
+					received = this.clients.getReader().readLine();
 				}catch(IOException e)
 				{
 					e.printStackTrace();
 				}
-			}
+			
+			String[] parts = received.split(SEPARATOR);
+			if(parts[0].equals("Request"))
+				switch(parts[1])
+				{
+				case "move":
+					this.gameController.moveRequest(Integer.parseInt(parts[2]), new Coordinate(Integer.parseInt(parts[3]), Integer.parseInt(parts[4])));
+					break;
+				case "spell":
+					this.gameController.spellRequest(Integer.parseInt(parts[2]),Integer.parseInt(parts[3]) , new Coordinate(Integer.parseInt(parts[4]), Integer.parseInt(parts[5])));
+					break;
+				case "action":
+					for(StatusMessages Smsg : StatusMessages.values())
+						if(Smsg.name().equals(parts[3]))
+							this.gameController.actionRequest(Integer.parseInt(parts[2]), Smsg);
+					break;
+				case "page":
+				//use the one from GameClient	
+					break;
+				}
 		}
 	}
 	
@@ -49,8 +75,8 @@ public class GameClientHandler implements GameView
 	{
 		try
 		{
-			this.clients[index].getWriter().append(msg);
-			this.clients[index].getWriter().flush();
+			this.clients.getWriter().append(msg);
+			this.clients.getWriter().flush();
 		} catch (IOException e)
 		{
 			e.printStackTrace();
@@ -59,50 +85,31 @@ public class GameClientHandler implements GameView
 	@Override
 	public void askActionChoice(int currentPlayerIndex)
 	{
-
+		this.send("Ask" +GameClient.WORD_SEPARATOR +"actionChoice" + GameClient.WORD_SEPARATOR+currentPlayerIndex, currentPlayerIndex);
 	}
 
-	@Override
 	public void askSpell(int currentPlayerIndex)
 	{
-		
+		this.send("Ask"+GameClient.WORD_SEPARATOR+"spell"+GameClient.WORD_SEPARATOR+currentPlayerIndex, currentPlayerIndex);
 	}
 
 	@Override
 	public void askPageSelection(int currentPlayerIndex)
 	{
-		
+		this.send("Ask"+GameClient.WORD_SEPARATOR+"pageSelection"+GameClient.WORD_SEPARATOR+currentPlayerIndex, index);
 	}
 
-	@Override
-	public void askChoiceMenu(int currentPlayerIndex)
-	{
-		
-	}
-
-	@Override
-	public String askPageName()
-	{
-		return null;
-	}
-
-	@Override
-	public Effect askSpellElement()
-	{
-		return null;
-	}
 
 	@Override
 	public void askMove(int currentPlayerIndex)
 	{
-		
+		this.send("Ask"+GameClient.WORD_SEPARATOR+"move"+currentPlayerIndex,currentPlayerIndex);
 	}
 
 	@Override
 	public void displaySpellSelection()
 	{
-		// TODO Auto-generated method stub
-		
+		this.send("Display"+GameClient.WORD_SEPARATOR+"spellSelection", 1);
 	}
 
 	@Override
@@ -111,11 +118,6 @@ public class GameClientHandler implements GameView
 		
 	}
 
-	@Override
-	public void displayMenu()
-	{
-		
-	}
 
 	@Override
 	public void displayStatus(StatusMessages msg)
@@ -123,11 +125,6 @@ public class GameClientHandler implements GameView
 		
 	}
 
-	@Override
-	public void displaySpellPage(ArrayList<SpellPage> listPages)
-	{
-		
-	}
 
 	@Override
 	public void displayChoiceAction()
@@ -141,17 +138,6 @@ public class GameClientHandler implements GameView
 		
 	}
 
-	@Override
-	public void displayElementChoice()
-	{
-		
-	}
-
-	@Override
-	public void displayShapeChoice()
-	{
-		
-	}
 
 	@Override
 	public void displayNextTurn(int numPlayer)
@@ -168,7 +154,7 @@ public class GameClientHandler implements GameView
 	@Override
 	public void displaySpellPageDetail(SpellPage pPage)
 	{
-		
+//		this.send("Display"+GameClient.WORD_SEPARATOR+"pageDetail"+GameClient.WORD_SEPARATOR+pPage.getPageName()+GameClient.GROUP_SEPARATOR+pPage.getSpell(0).getShape().getName()+GameClient.WORD_SEPARATOR+pPage.getSpell(0).getShape().getDamage()+GameClient.WORD_SEPARATOR+pPage.getSpell(0).getShape().getCooldown()+GameClient.WORD_SEPARATOR+pPage.getSpell(0).getShape().getRange()+GameClient.WORD_SEPARATOR+pPage.getSpell(0).getShape().get, 0);
 	}
 
 	@Override
@@ -177,15 +163,25 @@ public class GameClientHandler implements GameView
 		
 	}
 
-	@Override
-	public void displaySelectForThisPawn(Pawn thePawn)
-	{
-		
-	}
 
 	@Override
 	public void displayMoveSelection()
 	{
+		
+	}
+
+
+	@Override
+	public void displaySpellPage(ArrayList<SpellPage> listPages)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displaySelectForThisPawn(Pawn thePawn)
+	{
+		// TODO Auto-generated method stub
 		
 	}
 
