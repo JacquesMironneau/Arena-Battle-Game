8cbd74bbeb64d6acfd94392c944c951848170cec
jules chapelle <jules chapelle@LAPTOP-GNEHCQ45>
2019-06-11 16:37:51 +0100

    GameClient request,ask and display method done

--- S2-projet/src/fr/iutvalence/projet/battleArenaGame/network/GameClient.java
+++ S2-projet/src/fr/iutvalence/projet/battleArenaGame/network/GameClient.java
@@ -23,6 +23,10 @@ import fr.iutvalence.projet.battleArenaGame.view.StatusMessages;
 public class GameClient implements GameController
 {
 
+	public static final String WORD_SEPARATOR ="§";
+	public static final String GROUP_SEPARATOR ="&";
+	public static final String SENTENCE_SEPARATOR ="#";
+	
 	private GameView gameView;
 	
 	private Socket socket;
@@ -43,33 +47,44 @@ public class GameClient implements GameController
 		}).start();
 	}
 
+	private void send(String msg,int currentPlayerIndex)
+	{
+		try {
+			writer.append(msg);
+			writer.flush();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 	
 	@Override
 	public void moveRequest(int currentPlayerIndex, Coordinate destination)
 	{
-		String request = "Request move " + currentPlayerIndex + " " + destination.getCoordX() + " " +destination.getCoordY();
-		send(request);
+		String request = "Request move " + currentPlayerIndex + GameClient.WORD_SEPARATOR + destination.getCoordX() + GameClient.WORD_SEPARATOR +destination.getCoordY();
+		send(request,currentPlayerIndex);
 	}
 
 	@Override
 	public void spellRequest(int currentPlayerIndex, int spellIndex, Coordinate destination)
 	{
-		String request = "Request spell "+currentPlayerIndex+" "+spellIndex+" "+destination.getCoordX() + " " +destination.getCoordY();
-		send(request);
+		String request = "Request spell "+currentPlayerIndex+GameClient.WORD_SEPARATOR+spellIndex+GameClient.WORD_SEPARATOR+destination.getCoordX() + GameClient.WORD_SEPARATOR +destination.getCoordY();
+		send(request,currentPlayerIndex);
 	}
 
 	@Override
 	public void setPageRequest(int currentPlayerIndex, SpellPage pageToSet)
 	{
-		String request = "Request page "+currentPlayerIndex+ " "+ pageToSet.getPageName()+ " ";
+		String request = "Request page "+currentPlayerIndex+ GameClient.WORD_SEPARATOR+ pageToSet.getPageName()+ GameClient.WORD_SEPARATOR;
 		for(Spell s: pageToSet.getSpell())
 		{
-			request += s.getShape().getName()+ " " + s.getShape().getDamage()+ " "+s.getShape().getCooldown()+" "+s.getShape().getRange()+ " " + s.getShape().getSpellCost()+ " "+ s.getShape().getEffectedCoordinates().size()+ " ";
+			request += s.getShape().getName()+ GameClient.WORD_SEPARATOR + s.getShape().getDamage()+ GameClient.WORD_SEPARATOR+s.getShape().getCooldown()+GameClient.WORD_SEPARATOR+s.getShape().getRange()+ GameClient.WORD_SEPARATOR + s.getShape().getSpellCost()+ GameClient.WORD_SEPARATOR+ s.getShape().getEffectedCoordinates().size()+ GameClient.WORD_SEPARATOR;
 			for(Coordinate c: s.getShape().getEffectedCoordinates())
-				request += c.getCoordX()+ " " + c.getCoordY()+ " ";
-			request += s.getSpellEffect().getEffectName() + " ";
+				request += c.getCoordX()+ GameClient.WORD_SEPARATOR + c.getCoordY()+ GameClient.WORD_SEPARATOR;
+			request += s.getSpellEffect().getEffectName() + GameClient.WORD_SEPARATOR;
 		}
-		send(request);
+		send(request,currentPlayerIndex);
 
 	}
 
@@ -90,8 +105,8 @@ public class GameClient implements GameController
 			break;
 		}
 		
-		String request = "Request action "+currentPlayerIndex+" "+str;
-		send(request);
+		String request = "Request action "+currentPlayerIndex+GameClient.WORD_SEPARATOR+str;
+		send(request,currentPlayerIndex);
 	}
 	
 	public void send(String msg)
@@ -114,87 +129,131 @@ public class GameClient implements GameController
 			String receivedFrame = null;
 			
 			try {
-				this.reader.readLine();
+				receivedFrame=this.reader.readLine();
 			}catch(IOException e)
 			{
 				e.printStackTrace();
 			}
 
-			String[] parts = receivedFrame.split(" ");
-			
+			String[] parts = receivedFrame.split("GameClient.WORD_SEPARATOR");
 			switch(parts[0])
 			{
 			case "Ask":
 				switch(parts[1])
 				{
-				case "askChoice":
+				case "actionChoice":
+					this.gameView.askActionChoice(Integer.parseInt(parts[2]));
 					break;
-//				case
+				case "spell":
+					this.gameView.askSpell(Integer.parseInt(parts[2]));
+					break;
+				case "move":
+					this.gameView.askMove(Integer.parseInt(parts[2]));
+					break;
+				case "spellSelection":
+					this.gameView.askPageSelection(Integer.parseInt(parts[2]));
+					break;
+					
 				}
 				break;
 			case "Display":
 				switch(parts[1])
 				{
 				case "board":
-					String[] pawnPart = receivedFrame.split("#");
+					String[] pawnPart = receivedFrame.split(GameClient.SENTENCE_SEPARATOR);
+					parts = pawnPart[0].split(GameClient.WORD_SEPARATOR);
 					Board b = new Board(Integer.parseInt(parts[2]),Integer.parseInt(parts[4]),Integer.parseInt(parts[3]));
-					int pawnIndex = Integer.parseInt(parts[5]);
+					b.setCurrentPawnIndex(Integer.parseInt(parts[5]));
 					ArrayList<Pawn> newTurnOrder = new ArrayList<Pawn>();
 					for(int i=1;i<= Integer.parseInt(parts[6]);i++)
 					{
-						Pawn p = new Pawn(Integer.parseInt(parts[0]),new Coordinate(Integer.parseInt(pawnPart[1]),Integer.parseInt(pawnPart[2])),pawnPart[3]);
-						p.setHealthPoints(Integer.parseInt(pawnPart[4]));
-						p.setMovePoints(Integer.parseInt(pawnPart[5]));
-						p.setActionPoints(Integer.parseInt(pawnPart[6]));
-						SpellPage s = new SpellPage(pawnPart[7]);
-						String[] spellPart = pawnPart[i].split("$");
-						for(i=1;i<=3;i++)
+						String[] parts2 = pawnPart[i].split(GameClient.WORD_SEPARATOR);
+						Pawn p = new Pawn(Integer.parseInt(parts2[0]),new Coordinate(Integer.parseInt(parts2[1]),Integer.parseInt(parts2[2])),parts2[3]);
+						p.setHealthPoints(Integer.parseInt(parts2[4]));
+						p.setMovePoints(Integer.parseInt(parts2[5]));
+						p.setActionPoints(Integer.parseInt(parts2[6]));
+						SpellPage s = new SpellPage(parts2[7],null,null,null);
+						String[] spellPart = pawnPart[i].split(GameClient.GROUP_SEPARATOR);
+						for(int m=1;m<=3;m++)
 						{
-							String[] shapePart = spellPart[i].split("&");
+							parts2 = spellPart[m].split(GameClient.WORD_SEPARATOR);
 							HashSet<Coordinate> myCoordinate = new HashSet<Coordinate>();
-							s.getSpell(i-1).setShape(new Shape(shapePart[0],Integer.parseInt(shapePart[1]),Integer.parseInt(shapePart[2]),Integer.parseInt(shapePart[3]),Integer.parseInt(shapePart[4]),myCoordinate));
-							for(int k=0;k<Integer.parseInt(shapePart[5]);k++)
+							for(int k=0;k<Integer.parseInt(parts2[6]);k++)
 							{
-								s.getSpell(i-1).getShape().getEffectedCoordinates().add(new Coordinate(Integer.parseInt(shapePart[7+k*2]),Integer.parseInt(shapePart[8+k*2])));	
+								myCoordinate.add(new Coordinate(Integer.parseInt(parts2[7+k*2]),Integer.parseInt(parts[8+k*2])));
 							}
+							Spell spell = new Spell();
+							spell.setShape(new Shape(parts2[0],Integer.parseInt(parts2[1]),Integer.parseInt(parts2[2]),Integer.parseInt(parts2[3]),Integer.parseInt(parts2[4]),myCoordinate));
+							spell.setCurrentCooldown(0);
+							s.setSpell(i-1,spell);
 							for(Effect eff : Effect.values())
 							{
-								if(eff.getElementName().equals(Integer.parseInt(shapePart[6])))
+								if(eff.getElementName()==parts2[5]);
 									s.getSpell(i-1).setSpellEffect(eff);
 							}
+						}
+							parts2 = spellPart[4].split(GameClient.WORD_SEPARATOR);
 							p.setSpellPage(s);
 							ArrayList<PawnEffect> ef = new ArrayList<PawnEffect>();
-							for(int k=0;k<Integer.parseInt(pawnPart[4]);k++)
+							for(int k=0;k<Integer.parseInt(parts2[0]);k++)
 							{
 								for(Effect eff : Effect.values())
-									if(eff.getElementName().equals(Integer.parseInt(shapePart[6])))
+									if(eff.getElementName().equals(Integer.parseInt(parts2[3+k*2])))
 										ef.add(new PawnEffect(eff));
 										ef.get(k).setCurrentDuration(Integer.parseInt(pawnPart[2+k*2]));
 							}
-						}
+						p.getEffect().addAll(ef);
 						newTurnOrder.add(p);
-								
 					}
-					b.setTurnOrder(newTurnOrder);
+					b.setTurnOrder(newTurnOrder);	
 					this.gameView.displayBoard(b,Integer.parseInt(parts[2]));
 					break;
 				case "status":
+					for(StatusMessages stat : StatusMessages.values())
+						if(stat.name()==parts[2])
+							{
+								this.gameView.displayStatus(stat);
+								break;
+							}
 					break;
 				case "end":
-					parts = receivedFrame.split(" ");
 					this.gameView.displayEnd(parts[2]);
 					break;
 				case "nextTurn":
-					parts = receivedFrame.split(" ");
 					this.gameView.displayNextTurn(Integer.parseInt(parts[2]));
 					break;
 				case "spellPageDetails":
+					String[] spellParts = receivedFrame.split(GameClient.GROUP_SEPARATOR);
+					SpellPage p = new SpellPage(parts[2],null,null,null);
+					for(int i=1;i<=3;i++)
+					{
+						String[] parts2 = spellParts[i].split(GameClient.WORD_SEPARATOR);
+						HashSet<Coordinate> myCoordinate = new HashSet<Coordinate>();
+						for(int k=0;k<Integer.parseInt(parts2[6]);k++)
+						{
+							myCoordinate.add(new Coordinate(Integer.parseInt(parts2[7+k*2]),Integer.parseInt(parts[8+k*2])));
+						}
+						Spell spell = new Spell();
+						spell.setShape(new Shape(parts2[0],Integer.parseInt(parts2[1]),Integer.parseInt(parts2[2]),Integer.parseInt(parts2[3]),Integer.parseInt(parts2[4]),myCoordinate));
+						spell.setCurrentCooldown(0);
+						p.setSpell(i-1,spell);
+						for(Effect eff : Effect.values())
+						{
+							if(eff.getElementName()==parts2[5]);
+								p.getSpell(i-1).setSpellEffect(eff);
+						}
+						this.gameView.displaySpellPageDetail(p);
+					}
+					this.gameView.displaySpellPageDetail(p);
 					break;
 				case "selectForThisPawn":
+					this.gameView.displaySelectForThisPawn(parts[2]);
 					break;
 				case "spellPages":
+					this.gameView.displaySpellPage();
 					break;
 				case "choiceAction":
+					this.gameView.displayChoiceAction();
 					break;
 				}
 				break;
@@ -202,5 +261,6 @@ public class GameClient implements GameController
 		}
 		
 	}
+
 	
 }
